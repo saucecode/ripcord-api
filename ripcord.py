0c0a805a2d21d31e66ec02f125c987cbbde5a7d1
Julian <cahill.julian@gmail.com>
2017-05-04 07:08:32 +1200

    added discord websocket listening functionality -- program now listens/responds to pings

--- ripcord.py
+++ ripcord.py
@@ -1,12 +1,22 @@
+from websocket import create_connection
 
-import requests, json
+import requests, json, threading, select, multiprocessing, time, datetime
+
 
 class DiscordClient:
 	def __init__(self):
 		self.login_url = 'https://discordapp.com/api/v6/auth/login'
 		self.me_url = 'https://discordapp.com/api/v6/users/@me'
 		self.gateway_url = 'https://discordapp.com/api/v6/gateway'
+
+		self.ws_gateway_query_params = '/?encoding=json&v=6'
+
 		self.headers = {'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0'}
+
+		self.ws = None
+		self.ws_send_queue = multiprocessing.Queue()
+
+		self.message_counter = 0
 
 	def login(self, email : str, password : str):
 		""" Attempts to login with the given credentials.
@@ -54,6 +64,41 @@ class DiscordClient:
 			return data
 		return req
 
+	def connect_websocket(self, gateway_url : str):
+		# do connect to websocket url gateway_url
+		self.ws = create_connection(gateway_url + self.ws_gateway_query_params)
+
+		self.ws_thread = threading.Thread(target=self.websocket_loop)
+		self.ws_ping_thread = threading.Thread(target=self.websocket_ping)
+		self.ws_thread.start()
+		print('Started websocket thread\n')
+
+	def websocket_loop(self):
+		while 1:
+			readable, writable, executable = select.select( [self.ws, self.ws_send_queue._reader], [], [] )
+
+			for item in readable:
+				if item == self.ws:
+					self.ws_recv_callback(self.ws.recv())
+
+				elif item == self.ws_send_queue._reader:
+					self.ws.send( self.ws_send_queue.get() )
+
+	def websocket_ping(self):
+		while 1:
+			time.sleep(self.heartbeat_interval)
+			ping_packet = json.dumps( {'op':1, 'd':self.message_counter} )
+			self.websocket_send( ping_packet )
+			print('Sent ping: %s' % ping_packet)
+
+
+	def websocket_send(self, data : bytes):
+		self.ws_send_queue.put(data)
+
+	def websocket_received_callback(self, callback):
+		# call the callback function when receiving a packet
+		self.ws_recv_callback = callback
+
 
 if __name__ == '__main__':
 	client = DiscordClient()
@@ -61,9 +106,63 @@ if __name__ == '__main__':
 	with open('credentials','r') as f:
 		email,password = json.load(f)
 
+	# do login
 	print(client.login( email, password ))
+	print(client.token)
+
+	# download @me data
 	print(client.get_me())
 	print(client.me)
-	print(client.retrieve_websocket_gateway())
-	with open('dump.json','w') as f:
-		json.dump(client.download_messages('181226314810916865'), f)
+
+	# get the gateway
+	websocket_url = client.retrieve_websocket_gateway()
+	print(websocket_url)
+
+	# create websocket callback
+	def ws_callback(message):
+		print('RECV %s' % message)
+		client.message_counter += 1
+
+		if type(message) == str:
+			data = json.loads(message)
+			if data['op'] == 10:
+				client.heartbeat_interval = data['d']['heartbeat_interval'] / 1000
+				client.ws_ping_thread.start()
+				print('Started ping thread')
+
+			elif data['op'] == 11: # do not count pings!
+				client.message_counter -= 1
+
+		else:
+			pass
+
+	client.websocket_received_callback(ws_callback)
+
+	# start the websocket
+	client.connect_websocket(websocket_url)
+	client.websocket_send(json.dumps({
+		"op": 2,
+		"d": {
+			"token": client.token,
+			"properties": {
+				"os": "Linux",
+				"browser": "Firefox",
+				"device": "",
+				"referrer": "",
+				"referring_domain": ""
+			},
+			"large_threshold": 100,
+			"synced_guilds": [],
+			"presence": {
+				"status": "online",
+				"since": 0,
+				"afk": False,
+				"game": None
+			},
+			"compress": True
+		}
+	}))
+
+	client.websocket_send(json.dumps(
+		{"op":4,"d":{"guild_id":None,"channel_id":None,"self_mute":True,"self_deaf":False,"self_video":False}}
+	))
