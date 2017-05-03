5f4c83ae4b66a3c6ae0a4258838768df046d7ba6
Julian <cahill.julian@gmail.com>
2017-05-04 04:45:58 +1200

    added docstrings. added websocket gateway retrieval function. added download_messages function to get historical logs per channel

--- ripcord.py
+++ ripcord.py
@@ -5,9 +5,13 @@ class DiscordClient:
 	def __init__(self):
 		self.login_url = 'https://discordapp.com/api/v6/auth/login'
 		self.me_url = 'https://discordapp.com/api/v6/users/@me'
+		self.gateway_url = 'https://discordapp.com/api/v6/gateway'
 		self.headers = {'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0'}
 
 	def login(self, email : str, password : str):
+		""" Attempts to login with the given credentials.
+		Returns true on sucess, and stores the authtoken in self.token """
+
 		data = json.dumps({'email': email, 'password':password}).encode('utf-8')
 
 		print('Attempting login of', email, ':', '*'*len(password))
@@ -20,6 +24,10 @@ class DiscordClient:
 		return False
 
 	def get_me(self):
+		""" Downloads information about the client.
+		Returns true if successful, and stores this information in a dict in self.me
+		"""
+
 		req = requests.get(self.me_url, headers={**self.headers, 'Authorization':self.token})
 		if req.status_code == 200:
 			data = req.json()
@@ -29,6 +37,23 @@ class DiscordClient:
 			return True
 		return False
 
+	def retrieve_websocket_gateway(self):
+		""" Attempts to get the websocket URL. Returns that URL. """
+		req = requests.get(self.gateway_url, headers={**self.headers, 'Authorization':self.token})
+		if req.status_code == 200:
+			data = req.json()
+			return data['url']
+		return False
+
+	def download_messages(self, channelid : str, limit=50):
+		""" Downloads messages for a specific channel id. Must have an authtoken. """
+		request_url = 'https://discordapp.com/api/v6/channels/{}/messages?limit={}'.format(channelid, limit)
+		req = requests.get(request_url, headers={**self.headers, 'Authorization':self.token})
+		if req.status_code == 200:
+			data = req.json()
+			return data
+		return req
+
 
 if __name__ == '__main__':
 	client = DiscordClient()
@@ -37,3 +62,8 @@ if __name__ == '__main__':
 		email,password = json.load(f)
 
 	print(client.login( email, password ))
+	print(client.get_me())
+	print(client.me)
+	print(client.retrieve_websocket_gateway())
+	with open('dump.json','w') as f:
+		json.dump(client.download_messages('181226314810916865'), f)
